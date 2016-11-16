24d6ef24c0374c243a05b63f4b707f72a1e27455
xuchaozheng <github.123>
2016-11-16 16:42:47 +0800

    去掉websocket部分代码

--- client/js/page/index.js
+++ client/js/page/index.js
@@ -5,24 +5,13 @@ require(["/js/page/demo.js","jquery","/js/common/scope.js"], function (d,$,Scope
     new Scope({
         el:'body',
         events: {
-            'click .aa':'clickMe'
+            'click .main':'clickMe'
         },
         clickMe: function (e) {
-            console.log(e)
+            alert('click logo');
         },
         init: function () {
-            console.log('this is bodyddd');
-            var socket = new WebSocket('ws://localhost:3000/');
-            socket.onopen = function (event) {
-                // send a message to the server
-                socket.send('Hello server!');
-            };
-
-            socket.onmessage = function (event) {
-                // print message from server
-                console.log(event.data);
-
-            };
+            console.log('this is init');
         }
     });
 });
